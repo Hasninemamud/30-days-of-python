7e5994d729e14bbb12557ad30f3a0279bb478784
A.K.M Hasnine Mamud <95174908+Hasninemamud@users.noreply.github.com>
2023-08-09 12:53:32 +0600

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    sum = x + y
+    print(sum)
+add(2,4)
+
+#how to take input for function
+"""x = int(input("Enter the number: "))
+y = int(input("Enter the number: "))
+
+sum = x + y
+print(f"The sum of {x} and {y} is: {sum} ")"""
+
