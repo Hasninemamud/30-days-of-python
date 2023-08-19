c5cc7f93bbf77493d9016087c646e6db037b5450
Hasnine Mamud <hasninehemel85@gmail.com>
2023-08-19 12:13:12 +0600

    added regula expression

--- Regular exprassion.py
+++ Regular exprassion.py
@@ -20,11 +20,17 @@ pattern = r"colour"
 print(re.findall(pattern,"Red is a colour, I love red colour"))"""
 
 
-import re
+"""import re
 pattern = r"colour"
 text = "My favourite colour is red"
 match = re.search(pattern,text)
 if match:
     print(match.start())
     print(match.end())
-    print(match.span())
+    print(match.span())"""
+
+import re
+pattern = r"colour"
+text1 = "My favourite colour is red i love red colour"
+text2 = re.sub(pattern,"color",text1)
+print(text2)
