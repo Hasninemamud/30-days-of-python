039ce2e015db3b48b8957c09768a4142ca23dac1
Hasnine Mamud <hasninehemel85@gmail.com>
2023-08-26 01:44:40 +0600

    inital commit

---
+++ Alphabet.py
@@ -0,0 +1,10 @@
+"""for L in range(ord("a"), ord("z") + 1):
+    for x in range(1, L, +1):
+        print(chr(L))
+        print(x)
+"""
+letter = "Hasnine"
+for x in letter:
+    if x == "i" or x == "e":
+        break
+    print(x)
