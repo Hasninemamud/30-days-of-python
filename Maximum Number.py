c4cb7652592b1d066e5c8c00d2a4d75150d04ae9
Hasnine Mamud <hasninehemel85@gmail.com>
2023-08-25 23:57:07 +0600

    inital commit

--- Maximum Number.py
+++ Maximum Number.py
@@ -1,5 +1,18 @@
-Value = [22, 33, 99, 105, 12, 86]
+"""Value = [22, 33, 99, 105, 12, 86]
 
 value_sored = sorted(Value,reverse=True)
 second_highest = value_sored[1]
 print(second_highest)
+"""
+"""num = list(range(10))
+print(num)"""
+
+Value = [22, 33, 99, 105, 12, 86]
+print(Value)
+n = len(Value)
+index = 0
+
+while index < n:
+    print(Value[index])
+    index = index + 1
+
