5464e0e2a21b4d8e89783ca9001849158a9a83e6
Hasnine Mamud <hasninehemel85@gmail.com>
2023-08-27 23:47:57 +0600

    inital commit

--- practice 2.py
+++ practice 2.py
@@ -1,4 +1,4 @@
-Matrix = [
+"""Matrix = [
     [1, 2, 3],
     [4, 5, 6]
 ]
@@ -7,4 +7,12 @@ print(Matrix [1][2])
 
 for row in Matrix:
     for col in row:
-        print(col)
+        print(col)"""
+
+# Dictionary
+student = {
+    101 : "Hasnine Mamud",
+    102 : "hasninehemel85@gmail.com",
+    103 : "01636441892"
+}
+print(student.get( 104,"Not a valid key"))
