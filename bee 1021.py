0623d4e92e73912878ba361529c4d12cf9ab0b95
Hasnine Mamud <hasninehemel85@gmail.com>
2023-10-11 20:30:15 +0600

    initial commit

---
+++ bee 1021.py
@@ -0,0 +1,27 @@
+"""n = float(input("Enter the value: "))
+a = n / 100
+g = n % 100
+b = n / 50
+h = n % 50
+c = n / 20
+i = n % 20
+d = n / 10
+j = n % 10
+e = n / 5
+k = n % 5
+f = n / 2
+l = n % 2
+print("Notas")
+print(f"{int(a)} nota(s) de R$ 100.00")
+print(f"{int(b)} nota(s) de R$ 50")
+print(f"{int(c)} nota(s) de R$ 20")
+print(f"{int(d)} nota(s) de R$ 10")
+print(f"{int(e)} nota(s) de R$ 5")
+print(f"{int(f)} nota(s) de R$ 2")
+
+print("MOEDAS:")
+"""
+name1 = "Python"
+name2 = "Develpoer"
+result = name1[3:] + name2[1:]
+print(result)
