090a7b402d903e6ece161f6f6e764bdcded8c13e
Hasnine Mamud <hasninehemel85@gmail.com>
2023-12-07 22:05:10 +0600

    initial commit

--- beecrowd 1074.py
+++ beecrowd 1074.py
@@ -1,17 +1,15 @@
-n = int(input())
+n=int(input())
 for i in range(n):
-    i = int(input())
-    if i < 0:
-        if i % 2 == 0:
+    a=int(input())
+    if(a<0):
+        if(a%2==0):
             print("EVEN NEGATIVE")
         else:
             print("ODD NEGATIVE")
-
-    if i > 0:
-        if i % 2 == 0:
+    elif(a>0):
+        if(a % 2 == 0):
             print("EVEN POSITIVE")
         else:
             print("ODD POSITIVE")
-
-    elif i == 0:
+    elif(a==0):
         print("NULL")
---
+++ beecrowd 1078.py
@@ -0,0 +1,4 @@
+n = int(input())
+for i in range(1,10+1,1):
+    if 1<n<1000:
+        print(f"{i} x {n} = {i*n}")
