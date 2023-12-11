8adabcd968de05ae3ac1a2ab0f8e42764c25ef83
Hasnine Mamud <hasninehemel85@gmail.com>
2023-12-11 23:44:56 +0600

    initial commit

---
+++ beecrowd 1080.py
@@ -0,0 +1,10 @@
+"""a = 0
+b = 0
+for i in range(10):
+    i = int(input())
+    if i > a:
+         a = i
+         b = i
+print(a)
+print(b + 1)
+"""
---
+++ beecrowd 1092.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+total = 0
+c = 0
+r = 0
+s = 0
+
+for i in range(n):
+    i,chr = list(map(str,input().split()))
+    i = int(i)
+    if (chr == "c"):
+        c += i
+    elif (chr == "r"):
+        r += i
+    elif (chr == "s"):
+        s += i
+total = c+r+s
+coelhos = (c*100)/total
+Rato = (r*100)/total
+Sapo = (s*100)/total
+print(f"Total: {total} cobaias")
+print(f"Total de coelhos: {c}")
+print(f"Total de ratos: {r}")
+print(f"Total de sapos: {s}")
+print(f"Percentual de coelhos: {coelhos:.2f}%")
+print(f"Percentual de ratos: {Rato:.2f}%")
+print(f"Percentual de sapos: {Sapo:.2f}%")
