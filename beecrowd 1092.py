9dc762468bc33d3ccddeaf64ccff523b5b315de7
Hasnine Mamud <hasninehemel85@gmail.com>
2023-12-11 23:47:34 +0600

    initial commit

--- beecrowd 1092.py
+++ beecrowd 1092.py
@@ -8,11 +8,11 @@ s = 0
 for i in range(n):
     i,chr = list(map(str,input().split()))
     i = int(i)
-    if (chr == "c"):
+    if (chr == "C"):
         c += i
-    elif (chr == "r"):
+    elif (chr == "R"):
         r += i
-    elif (chr == "s"):
+    elif (chr == "S"):
         s += i
 total = c+r+s
 coelhos = (c*100)/total
@@ -22,6 +22,6 @@ print(f"Total: {total} cobaias")
 print(f"Total de coelhos: {c}")
 print(f"Total de ratos: {r}")
 print(f"Total de sapos: {s}")
-print(f"Percentual de coelhos: {coelhos:.2f}%")
-print(f"Percentual de ratos: {Rato:.2f}%")
-print(f"Percentual de sapos: {Sapo:.2f}%")
+print(f"Percentual de coelhos: {coelhos:.2f} %")
+print(f"Percentual de ratos: {Rato:.2f} %")
+print(f"Percentual de sapos: {Sapo:.2f} %")
