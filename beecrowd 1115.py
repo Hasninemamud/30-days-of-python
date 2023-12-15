e8afc3a620c2806e9c08e01d99de7b59efa0f05f
Hasnine Mamud <hasninehemel85@gmail.com>
2023-12-15 12:08:39 +0600

    initial commit

---
+++ beecrowd 1115.py
@@ -0,0 +1,21 @@
+while(True):
+    try:
+        x,y = list(map(int,input().split()))
+        if x > 0 and y > 0:
+            print("primeiro")
+        elif (x > 0 and y < 0):
+            print("quarto")
+        elif (x < 0 and y < 0):
+            print("terceiro")
+        elif (x < 0 and y > 0):
+            print("segundo")
+        elif x == 0 or y == 0:
+            break
+    except EOFError:
+        break
+
+
+
+
+
+
